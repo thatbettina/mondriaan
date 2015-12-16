8e6dc47e7f4af2cc9aacdc5577d57a9846d38fae
Bettina <bettina.shzu@gmail.com>
2015-12-16 21:49:41 +0100

    implemented hline and vline

--- src/mondriaan/core.cljs
+++ src/mondriaan/core.cljs
@@ -7,12 +7,19 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:hlines [50 150 383]
+                          :vlines [50 200 350]}))
 
 (defn line [x1 y1 x2 y2]
   [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2 :stroke-width 6 :stroke "black"}])
 
+(defn hline [y]
+  (line 0 y 500 y)
+)
 
+(defn vline [x]
+  (line x 0 x 500)
+  )
 
 (defn rect [x y width height fill]
   [:rect {:x x :y y :width width :height height :fill fill}])
@@ -23,19 +30,7 @@
    [rect 350 150 65 233 "blue"]
    [rect 200 420 70 70 "yellow"]
 
-   ;; horizontal
-   ;;         y1      y2
-   [line 0    50 500  50]
-   [line 0   150 500 150]
-   [line 200 383 500 383]
-
-   ;; vertical
-   ;;    x1    x2
-   [line 50 50 50 500]
-   [line 350 0 350 500]
-   [line 200 0 200 500]
-
-   ])
+])
 
 (defn hello-world []
   [:div [:h1 "Mondriaan"]
