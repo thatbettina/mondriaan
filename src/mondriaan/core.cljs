75e42348ff9af49c0f05eded10b7009f3067a362
Arne Brasseur <arne@arnebrasseur.net>
2015-12-16 21:21:10 +0100

    Sort lines a bit

--- src/mondriaan/core.cljs
+++ src/mondriaan/core.cljs
@@ -12,20 +12,30 @@
 (defn line [x1 y1 x2 y2]
   [:line {:x1 x1 :y1 y1 :x2 x2 :y2 y2 :stroke-width 6 :stroke "black"}])
 
+
+
 (defn rect [x y width height fill]
   [:rect {:x x :y y :width width :height height :fill fill}])
 
 (defn canvas []
   [:svg {:width 500 :height 500}
-    [rect 50 50 100 100 "red"]
-    [rect 350 150 65 233 "blue"]
-    [rect 200 420 70 70 "yellow"]
-    [line 50 50 50 500]
-    [line 0 50 500 50]
-    [line 350 0 350 500]
-    [line 0 150 500 150]
-    [line 200 0 200 500]
-    [line 200 383 500 383]])
+   [rect 50 50 100 100 "red"]
+   [rect 350 150 65 233 "blue"]
+   [rect 200 420 70 70 "yellow"]
+
+   ;; horizontal
+   ;;         y1      y2
+   [line 0    50 500  50]
+   [line 0   150 500 150]
+   [line 200 383 500 383]
+
+   ;; vertical
+   ;;    x1    x2
+   [line 50 50 50 500]
+   [line 350 0 350 500]
+   [line 200 0 200 500]
+
+   ])
 
 (defn hello-world []
   [:div [:h1 "Mondriaan"]
